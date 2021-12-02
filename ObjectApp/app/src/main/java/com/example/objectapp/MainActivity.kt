2925760e4356cb21a73821a13a894ffa5068a769
jakubzolkos <zolkos@bu.edu>
2021-12-02 01:33:33 -0500

    Example text-to-speech functionality added

--- ObjectApp/app/src/main/java/com/example/objectapp/MainActivity.kt
+++ ObjectApp/app/src/main/java/com/example/objectapp/MainActivity.kt
@@ -2,40 +2,33 @@ package com.example.objectapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.widget.Toast
+import android.speech.tts.TextToSpeech
+import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
-    var handler: Handler = Handler()
-    var runnable: Runnable? = null
-    var delay = 1000 // Delay in seconds / 1000
-    var test = 1
+
+    lateinit var mTTS:TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    /** Function to send image */
-    private fun sendMessage() {
-        val text = "Running method time $test"
-        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
-        test++
-    }
+        mTTS = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR) {
+                mTTS.language = Locale.US
+            }
+        })
 
-    // https://www.tutorialspoint.com/how-to-run-a-method-every-10-seconds-in-android-using-kotlin
-    override fun onResume() {
-        handler.postDelayed(Runnable {
-            handler.postDelayed(runnable!!, delay.toLong())
-
-            // CALL OUR FUNCTION HERE
-            sendMessage()
-
-        }.also { runnable = it }, delay.toLong())
-        super.onResume()
-    }
-    override fun onPause() {
-        super.onPause()
-        handler.removeCallbacks(runnable!!)
+        button_speak.setOnClickListener {
+            val toSpeak = textEt.text.toString()
+            if (toSpeak != ""){
+                mTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+            }
+        }
     }
 }
+
+
